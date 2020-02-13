19b07707a8be2dc5a2e82e8dec25ad5004b5ff42
Payakorn Saksuriya <53273950+payakorn@users.noreply.github.com>
2020-02-13 17:11:15 -0500

    get predicted Newton's direction

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def generate_system(A, x, s):
     r3 = np.ones((n, 1))
     r4 = np.matmul(S, X)
     right_hand_side = np.block([[rc], [rb], [r3]])
-    return (matrix, rb)
+    return (matrix, right_hand_side)
 
 
 def check_optimality(A, b, c, s):
@@ -26,32 +26,55 @@ def check_optimality(A, b, c, s):
     return primal or dual or duality_gap
 
 
-# def predicted_newton_direction(matrix, )
+def predicted_newton_direction(A, x, s):
+    (matrix, right_hand_side) = generate_system(A, x, s)
+    print("det =", np.linalg.det(matrix))
+    direction = np.linalg.solve(matrix, right_hand_side)
+    delta_x = direction[0:n]
+    delta_y = direction[n : n + m]
+    delta_s = direction[m + n :]
+    return (delta_x, delta_y, delta_s)
+
+
+def convert_to_array(A, b, c):
+    A = np.asarray(A)
+    b = np.asarray([b]).T
+    c = np.asarray([c]).T
+    return (A, b, c)
+
+
+def initial_vector():
+    x = np.random.randint(10, size=(n, 1))
+    y = np.random.randint(10, size=(m, 1))
+    s = np.random.randint(10, size=(n, 1))
+    return (x, y, s)
+
+
+def predicted_stepsize(delta_x, delta_y, delta_s):
+    pass
+
 
 ## Define problem
-c = [-100, -125]
-A = [[3, 6], [8, 4], [10, 1]]
-b = [30, 44, 55]
-# c = np.matrix([-100, -125])
-# A = np.matrix([(3, 6), (8, 4)])
-# b = np.matrix([30, 44])
+c = [-100, -125, -20]
+A = [[3, 6, 8], [8, 4, 1]]
+b = [30, 44]
 
 ## convert to numpy array
-A = np.asarray(A)
-b = np.asarray([b]).T
-c = np.asarray([c]).T
+(A, b, c) = convert_to_array(A, b, c)
 
-## variables bound
-# x0_bounds = (0, 5)
-# x1_bounds = (0, 4)
-x0_bounds = (0, np.inf)
-x1_bounds = (0, np.inf)
 
-# res = linprog(c, A_ub=A, b_ub=b, \
-#               bounds=(x0_bounds, x1_bounds),
-#               options={"disp": True})
+def scipy_solve(A, b, c):
+    x0_bounds = (0, np.inf)
+    x1_bounds = (0, np.inf)
+    res = linprog(
+        c,
+        A_ub=A,
+        b_ub=b,
+        bounds=(x0_bounds, x1_bounds, x1_bounds),
+        options={"disp": True},
+    )
+    print(res)
 
-# print(res)
 
 # parameters
 n = np.size(A, 1)
@@ -63,15 +86,17 @@ e2 = 0.1
 e3 = 0.1
 
 # main
-
-# initial vectors
-x = np.ones((n, 1))
-y = np.ones((m, 1))
-s = np.ones((n, 1))
 k = 0
-
-print(check_optimality(A, b, c, s))
-
+(x, y, s) = initial_vector()
 (matrix, rb) = generate_system(A, x, s)
-print(rb.T, "\n")
+scipy_solve(A, b, c)
+(delta_x, delta_y, delta_s) = predicted_newton_direction(A, x, s)
+
+
+print(rb, "\n")
 print(matrix, "\n")
+print("the optimaility:", check_optimality(A, b, c, s))
+print("newton direction:\n", direction)
+print("delta x:\n", delta_x)
+print("delta y:\n", delta_y)
+print("delta s:\n", delta_s)
